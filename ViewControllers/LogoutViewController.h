52bb2eff3388b523c099b6dacc669423cdb02fad
Yonatan Desta <ydesta@fb.com>
2022-07-06 15:36:35 -0700

    Tweaked UI, implemented logout and user persistence sign in feature

---
+++ ViewControllers/LogoutViewController.h
@@ -0,0 +1,16 @@
+//
+//  LogoutViewController.h
+//  StalkFinance
+//
+//  Created by Yonatan Desta on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LogoutViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
